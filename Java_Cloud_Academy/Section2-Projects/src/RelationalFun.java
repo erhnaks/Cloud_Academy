330f632508bb69d59f341b6030bf4bef53862a73
erhnaks <erhnaks@hotmail.com>
2022-09-20 11:56:22 +0100

    Relation Operations

---
+++ Java_Cloud_Academy/Section2-Projects/src/RelationalFun.java
@@ -0,0 +1,38 @@
+public class RelationalFun {
+    public static void main (String[] args){
+
+/*
+The Relational Operators
+> Greater than
+>= Greater than or equal to
+< Less than
+<= Less than equal to
+== equal to
+!= not equal to
+ */
+
+        boolean myBool = true;
+        boolean yourBool = false;
+        int myAge = 37;
+        int yourAge = 20;
+        int bobsAge = 20;
+
+        System.out.println("myBool is " + myBool);
+        System.out.println("your boolean is " + yourBool);
+
+        // Relational operations;
+
+        boolean ageComparision = myAge > yourAge;
+        System.out.println("myage > yourAge? " + ageComparision);
+
+        ageComparision = yourAge > bobsAge;
+
+        System.out.println("Yourage > bobsage? " + ageComparision);
+
+        ageComparision = yourAge == bobsAge;
+
+        System.out.println("yourage == bobsage? " + ageComparision);
+
+
+    } //end of main
+}
