548ffdffac738ceb796691bdea46a8523b003468
erhnaks <erhnaks@hotmail.com>
2022-09-20 11:14:35 +0100

    Addition Operator

---
+++ Java_Cloud_Academy/Section2-Projects/src/AritmethicFun.java
@@ -0,0 +1,23 @@
+public class AritmethicFun {
+   public static void main (String[] args) {
+       /*
+       Arithmetic operators are;
+       +
+       -
+       *
+       /
+       % (Modulus)
+        */
+
+
+       int a = 10;
+       int b = 15;
+
+       int result = a + b;
+
+       System.out.println("Result is " + result);
+
+
+
+   } // end of main
+}
