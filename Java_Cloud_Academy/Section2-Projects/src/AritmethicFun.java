91f237ed3b22ed586c92e8d3975f3a36a0d85d53
erhnaks <erhnaks@hotmail.com>
2022-09-20 11:20:03 +0100

    Arithmetic Operators

--- Java_Cloud_Academy/Section2-Projects/src/AritmethicFun.java
+++ Java_Cloud_Academy/Section2-Projects/src/AritmethicFun.java
@@ -14,8 +14,16 @@ public class AritmethicFun {
        int b = 15;
 
        int result = a + b;
+       int difference = a - b ;
+       int product = a * b;
+       int quotient = a / b;
+       int remainder = a % b;
 
        System.out.println("Result is " + result);
+       System.out.println("Difference is " + difference);
+       System.out.println("Product number is " + product);
+       System.out.println("Quotient is " + quotient);
+       System.out.println("Remainder is " + remainder);
 
 
 
