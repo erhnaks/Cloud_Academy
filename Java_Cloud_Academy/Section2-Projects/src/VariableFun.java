f4328ab7c6427a14d4797ad720286ba940b05594
erhnaks <erhnaks@hotmail.com>
2022-09-20 10:20:02 +0100

    Variable

---
+++ Java_Cloud_Academy/Section2-Projects/src/VariableFun.java
@@ -0,0 +1,9 @@
+public class VariableFun {
+    public static void main(String[] args) {
+       int age;  // Declaration
+       age = 19;  // Initialization
+       String name = "Billy";
+
+       System.out.println(name + " is " + age + " years old.");
+    }
+}
