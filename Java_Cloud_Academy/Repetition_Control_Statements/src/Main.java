b450790384e22e837fa5723718b81f3c57c0c241
erhnaks <erhnaks@hotmail.com>
2022-09-21 13:04:48 +0100

    Do-While Loops

--- Java_Cloud_Academy/Repetition_Control_Statements/src/Main.java
+++ Java_Cloud_Academy/Repetition_Control_Statements/src/Main.java
@@ -3,10 +3,18 @@ public class Main {
 
         int count = 0;
 
-        while (count < 10) {
-            System.out.println(count);
-            count++;
-        }
+//        while (count < 10) {
+//            System.out.println(count);
+//            count++;
+//        }
+
+//        int count2 = 0;
+//
+//        do{
+//            System.out.println(count2);
+//            count2++;
+//        }
+//        while(count2 <10);
 
 
     }
