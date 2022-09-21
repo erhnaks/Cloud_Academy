acc696198eaef51234d4faf6e984fcc6a69bb052
erhnaks <erhnaks@hotmail.com>
2022-09-21 10:12:13 +0100

    for loop Intro

--- Java_Cloud_Academy/Control_Statements/src/ControlStatementIntro.java
+++ Java_Cloud_Academy/Control_Statements/src/ControlStatementIntro.java
@@ -1,13 +1,18 @@
 public class ControlStatementIntro {
     public static void main(String[] args) {
-        int age = 16;
+        int age = 20;
 
 
         if (age >= 16) {
             System.out.println("You can drive!");
         } else {
             System.out.println("You can not drive.");
-        }
+        } // end of if-else
+
+
+        for (int i = 1; i <= age; i++) {
+            System.out.println("Happy Birthday " + i);
+        } // enf of for
 
 
     } //end of main
