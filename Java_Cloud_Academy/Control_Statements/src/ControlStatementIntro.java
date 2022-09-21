b535ad1965ca12b520646794d952fd6fb39c3a1c
erhnaks <erhnaks@hotmail.com>
2022-09-21 09:51:52 +0100

    If-Else Intro

---
+++ Java_Cloud_Academy/Control_Statements/src/ControlStatementIntro.java
@@ -0,0 +1,14 @@
+public class ControlStatementIntro {
+    public static void main(String[] args) {
+        int age = 16;
+
+
+        if (age >= 16) {
+            System.out.println("You can drive!");
+        } else {
+            System.out.println("You can not drive.");
+        }
+
+
+    } //end of main
+}
