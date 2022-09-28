53a90421be0b4fcd86cae3fdf013fdc519cc085e
erhnaks <erhnaks@hotmail.com>
2022-09-28 11:24:08 +0100

    Wrapper Classes

---
+++ Java_Cloud_Academy/Arrays_and_ArrayLists/src/WrapperFun.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class WrapperFun {
+
+    public static void main (String[] args){
+
+        ArrayList<Integer> myAl = new ArrayList<>();
+
+        myAl.add(20);
+        myAl.add(30);
+
+        for(int i =0; i < myAl.size(); i++){
+            System.out.println(myAl.get(i));
+        } //end of for loop
+
+        for (int num : myAl){
+            System.out.println(num);
+        } // end of enhanced for loop
+
+
+        String someValue = "450";
+
+        int parsedValue = Integer.parseInt(someValue); // Parsing String value into an Integer
+
+        parsedValue += 10;
+
+        System.out.println(parsedValue);
+
+
+
+    } // end of main
+}
