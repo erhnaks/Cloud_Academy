8a03ff9fd82a46a33f2545506aa0c399b59c7b8a
erhnaks <erhnaks@hotmail.com>
2022-09-29 11:59:10 +0100

    String Builders

---
+++ Java_Cloud_Academy/Strings/StringMethods1/src/StringMethods_StringBuilder.java
@@ -0,0 +1,25 @@
+public class StringMethods_StringBuilder {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("John Doe");
+
+        System.out.println(sb);
+
+        sb.append(" is awesome");
+
+        System.out.println(sb);
+
+        sb.insert(5, "Nick ");
+        System.out.println(sb);
+
+        sb.replace(17, 25, "amazing");
+        System.out.println(sb);
+
+        sb.delete(5, 10);
+
+        System.out.println(sb);
+
+    } // end of main
+
+}
