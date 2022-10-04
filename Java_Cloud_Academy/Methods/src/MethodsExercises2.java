4322c9d79bc53e260bbcbc08a89b2752d8a08efd
erhnaks <erhnaks@hotmail.com>
2022-10-04 10:31:18 +0100

    Methods Exercises 2

---
+++ Java_Cloud_Academy/Methods/src/MethodsExercises2.java
@@ -0,0 +1,18 @@
+public class MethodsExercises2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(getResult("Hello", "World"));
+
+
+    }// end of main
+
+
+    public static String getResult(String str1, String str2) {
+
+        return str1 + " " + str2;
+
+    }
+
+
+}
