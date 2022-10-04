58fe81be90cf9e81a0c1cdc7c7cb7553ad5712c7
erhnaks <erhnaks@hotmail.com>
2022-10-04 09:58:31 +0100

    Method Exercise - 1

---
+++ Java_Cloud_Academy/Methods/src/MethodsExercises.java
@@ -0,0 +1,15 @@
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+
+        double result;
+
+        result = squared(5.7);
+        System.out.println(result);
+
+    } // end of main
+    public static double squared(double num) {
+        return num * num;
+    } // end of square
+}
+
