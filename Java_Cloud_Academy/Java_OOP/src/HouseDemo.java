1fae8f54eabc21b30c488ad824727aab431bacdd
erhnaks <erhnaks@hotmail.com>
2022-10-07 13:28:47 +0100

    Encapsulation in Java. House object constructor with setters and getters. Included with a print method!

--- Java_Cloud_Academy/Java_OOP/src/HouseDemo.java
+++ Java_Cloud_Academy/Java_OOP/src/HouseDemo.java
@@ -2,8 +2,8 @@ public class HouseDemo {
 
     public static void main(String[] args) {
 
-        House myHouse = new House(3,18,"White");
-        House yourHouse = new House(5, 30,"Green");
+        House myHouse = new House(3, 18, "White");
+        House yourHouse = new House(5, 30, "Green");
 
 //        myHouse.setNumOfFloors(3);
 //        myHouse.setNumWindows(6);
@@ -12,14 +12,20 @@ public class HouseDemo {
 //        yourHouse.setNumOfFloors(4);
 //        yourHouse.setNumWindows(12);
 //        yourHouse.setColour("Blue");
+//        System.out.println("Your House has a " + yourHouse.getNumOfFloors() + " floor, and it has " + yourHouse.getNumWindows()
+//                + " windows. It's exterior colour is " + yourHouse.getColour() + ".");
 
-        System.out.println("My House has a " + myHouse.getNumOfFloors() + " floor, and it has " + myHouse.getNumWindows()
-                + " windows. It's exterior colour is " + myHouse.getColour() + ".");
+        printHouse(myHouse);
+        printHouse(yourHouse);
+
+    } // end of main
+
+    public static void printHouse(House house) {
+
+        System.out.println("My House has a " + house.getNumOfFloors() + " floor, and it has " + house.getNumWindows()
+                + " windows. It's exterior colour is " + house.getColour() + ".");
 
         System.out.println();
 
-        System.out.println("Your House has a " + yourHouse.getNumOfFloors() + " floor, and it has " + yourHouse.getNumWindows()
-                + " windows. It's exterior colour is " + yourHouse.getColour() + ".");
-
-    } // end of main
+    }
 } // end of houseDemo
