f8afa65bdb9667db6dcc8a9d8578c8a7fada797f
erhnaks <erhnaks@hotmail.com>
2022-10-10 12:53:16 +0100

    Circle Demo Project

--- Java_Cloud_Academy/BankAccaountBalance_Project/src/Circle.java
+++ Java_Cloud_Academy/BankAccaountBalance_Project/src/Circle.java
@@ -16,5 +16,17 @@ public double getRadius(){
     return radius;
 }
 
+public void setRadius(double radius){
+    this.radius = radius;
+}
+
+public double circumference(){
+    return  2 * Math.PI * radius;
+}
+
+public double area(){
+    return Math.PI * Math.pow(radius, 2);
+}
+
 
 }
