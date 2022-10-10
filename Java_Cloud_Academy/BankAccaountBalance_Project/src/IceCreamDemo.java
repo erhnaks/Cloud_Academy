ee5cfd23b747b749c098019473dd725fe4b697fc
erhnaks <erhnaks@hotmail.com>
2022-10-10 12:43:39 +0100

    Ice Cream Demo Project

--- Java_Cloud_Academy/BankAccaountBalance_Project/src/IceCreamDemo.java
+++ Java_Cloud_Academy/BankAccaountBalance_Project/src/IceCreamDemo.java
@@ -1,19 +1,32 @@
 public class IceCreamDemo {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        IceCream myIc = new IceCream("Choco", 3 , 2);
-        IceCream yourIc = new IceCream("Vanny", 4,3);
+        IceCream myIc = new IceCream("Choco", 3, 2);
+        IceCream yourIc = new IceCream("Vanny", 4, 3);
 
         myIc.addTopping("Strawberry");
         myIc.addTopping("Chocolate");
 
         yourIc.addTopping("Vanilla");
-        yourIc.addTopping("Springles");
+        yourIc.addTopping("Sprinkles");
+
+        printIc(myIc);
+        System.out.println();
+        printIc(yourIc);
+
 
     }// end of main
 
 
+    public static void printIc(IceCream iceCream) {
+
+        System.out.println("You ice cream is " + iceCream.getName() + " and it has number of " + iceCream.getNumScoops() + " scoops." +
+                "It cost: £" + iceCream.getCost());
+
+        System.out.println("Toppings are");
+
+        iceCream.printToppings();}
 
 
 
-}
+}// end of main
