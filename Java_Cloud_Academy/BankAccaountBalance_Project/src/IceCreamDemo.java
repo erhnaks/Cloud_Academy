afcb0365a5821840a261dd573d45ffce6c9be7fc
erhnaks <erhnaks@hotmail.com>
2022-10-10 12:37:52 +0100

    Ice Cream Demo Project

--- Java_Cloud_Academy/BankAccaountBalance_Project/src/IceCreamDemo.java
+++ Java_Cloud_Academy/BankAccaountBalance_Project/src/IceCreamDemo.java
@@ -1,2 +1,19 @@
 public class IceCreamDemo {
+    public static void main (String[] args){
+
+        IceCream myIc = new IceCream("Choco", 3 , 2);
+        IceCream yourIc = new IceCream("Vanny", 4,3);
+
+        myIc.addTopping("Strawberry");
+        myIc.addTopping("Chocolate");
+
+        yourIc.addTopping("Vanilla");
+        yourIc.addTopping("Springles");
+
+    }// end of main
+
+
+
+
+
 }
