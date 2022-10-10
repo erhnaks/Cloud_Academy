ee8dd87d14adb51d6a2540db42b5aa53a347320e
erhnaks <erhnaks@hotmail.com>
2022-10-10 12:59:03 +0100

    Circle Demo Project

--- Java_Cloud_Academy/BankAccaountBalance_Project/src/CircleDemo.java
+++ Java_Cloud_Academy/BankAccaountBalance_Project/src/CircleDemo.java
@@ -2,5 +2,21 @@ public class CircleDemo {
     public static void main (String[] args){
 
 
+        Circle myCircle = new Circle(2.5);
+
+        printCircleData(myCircle);
+
+
     } // end of main
+
+
+    public static void printCircleData(Circle circle){
+        System.out.println("r = " + circle.getRadius());
+        System.out.println("C = " + String.format("%.2f",circle.circumference())); // print 2 decimal places
+        System.out.println("A = " + String.format("%.2f",circle.area()));
+        System.out.println();
+    }
+
+
+
 }
