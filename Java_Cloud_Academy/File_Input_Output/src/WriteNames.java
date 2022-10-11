8922b1b48822ac86e354028439fbf45d107747d6
erhnaks <erhnaks@hotmail.com>
2022-10-11 11:10:19 +0100

    File Output Exercise

---
+++ Java_Cloud_Academy/File_Input_Output/src/WriteNames.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class WriteNames {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Eric");
+        names.add("Johnny");
+        names.add("Sunny");
+        names.add("Donny");
+        names.add("Pony");
+
+        System.out.println(names);
+
+        try {
+            PrintWriter pw = new PrintWriter("names.txt");
+
+            pw.println(names);
+
+            pw.close();
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("File is not found!");
+            System.out.println(ex.getMessage());
+        }
+
+
+    } // end of main
+
+}
